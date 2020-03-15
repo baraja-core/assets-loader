ac80d1360fcaeefffc62ad738d7e6fbe41a04d3a
Jan Barášek <janbarasek@gmail.com>
2020-03-15 09:52:02 +0100

    #1 Add support for Wildcard asset registration

--- src/LoaderExtension.php
+++ src/LoaderExtension.php
@@ -7,26 +7,23 @@ namespace Baraja\AssetsLoader;
 
 use Nette\Application\Application;
 use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\ServiceDefinition;
 use Nette\PhpGenerator\ClassType;
 
-class LoaderExtension extends CompilerExtension
+final class LoaderExtension extends CompilerExtension
 {
 
-	private const PROPERTY_NAME = 'baraja_assets_loader';
-
-	/**
-	 * @param ClassType $class
-	 */
-	public function afterCompile(ClassType $class): void
+	public function beforeCompile(): void
 	{
 		$assets = [];
 		$files = $this->getConfig()['routing'] ?? [];
 
 		if (isset($this->getConfig()['base']) === true) {
-			$files = array_merge($files, ['base' => $this->getConfig()['base']]);
+			$files = array_merge_recursive($files, ['*' => $this->getConfig()['base']]);
 		}
 
 		foreach ($files as $route => $assetFiles) {
+			$this->validateRouteFormat($route);
 			foreach ($assetFiles as $assetFile) {
 				if (preg_match('/^(?<name>.+)\.(?<format>[a-zA-Z0-9]+)$/', $assetFile, $fileParser)) {
 					if (isset($assets[$route][$fileParser['format']]) === false) {
@@ -40,18 +37,16 @@ class LoaderExtension extends CompilerExtension
 			}
 		}
 
-		$class->addProperty(self::PROPERTY_NAME, $assets)
-			->setVisibility('private');
+		/** @var ServiceDefinition $definition */
+		$definition = $this->getContainerBuilder()->getDefinitionByType(Api::class);
+		$definition->addSetup('?->setData(?)', ['@self', $assets]);
+	}
 
-		$class->addMethod('getBarajaAssetsLoader')
-			->setReturnType('array')
-			->setBody('return $route === null '
-				. '? $this->' . self::PROPERTY_NAME . ' '
-				. ': array_merge_recursive($this->' . self::PROPERTY_NAME . '[$route] ?? [], $this->' . self::PROPERTY_NAME . '[\'base\'] ?? []);')
-			->addParameter('route', null)
-			->setType('string')
-			->setNullable(true);
-
+	/**
+	 * @param ClassType $class
+	 */
+	public function afterCompile(ClassType $class): void
+	{
 		$class->getMethod('initialize')->addBody(
 			'if (strncmp($assetsLoader__basePath = ' . Helpers::class . '::processPath($this->getService(\'http.request\')), \'assets/web-loader/\', 18) === 0) {'
 			. "\n\t" . '$this->getByType(' . Application::class . '::class)->onStartup[] = function(' . Application::class . ' $a) use ($assetsLoader__basePath) {'
@@ -59,6 +54,20 @@ class LoaderExtension extends CompilerExtension
 			. "\n\t" . '};'
 			. "\n" . '}'
 		);
+	}
+
+	/**
+	 * @param string $route
+	 */
+	private function validateRouteFormat(string $route): void
+	{
+		if ($route === '*') { // special case for global assets
+			return;
+		}
+
+		if (preg_match('/^[A-Z0-9][A-Za-z0-9]*:(?:\*|[A-Z0-9][A-Za-z0-9]*:(?:\*|[a-z0-9][A-Za-z0-9]*))$/', trim($route, ':')) === 0) {
+			AssetLoaderException::routeIsInInvalidFormat($route);
+		}
 	}
 
 }
