4f7a08c19f671b07587e079cea896bf3332448d6
Jan Barášek <janbarasek@gmail.com>
2020-11-11 12:35:29 +0100

    Big modernize: Define service in extension without NEON definition + more ways to configure + custom inject tags, response HTTP headers, base path and more. Best performance: Full configuration is stored in DIC as static array.

--- src/Api.php
+++ src/Api.php
@@ -7,27 +7,29 @@ namespace Baraja\AssetsLoader;
 
 final class Api
 {
-	/** @var string[] */
-	private static array $formatHeaders = [
-		'js' => 'application/json',
-		'css' => 'text/css',
-	];
+	private string $basePath;
+
+	/** @var mixed[] */
+	private array $data;
 
 	/** @var string[] */
-	private static array $formatHtmlInjects = [
-		'js' => '<script src="%path%"></script>',
-		'css' => '<link href="%path%" rel="stylesheet">',
-	];
+	private array $formatHeaders;
 
-	private string $wwwDir;
-
-	/** @var mixed[]|null */
-	private ?array $data;
+	/** @var string[] */
+	private array $formatHtmlInjects;
 
 
-	public function __construct(string $wwwDir)
+	/**
+	 * @param mixed[] $data
+	 * @param string[] $formatHeaders
+	 * @param string[] $formatHtmlInjects
+	 */
+	public function __construct(string $basePath, array $data, array $formatHeaders, array $formatHtmlInjects)
 	{
-		$this->wwwDir = $wwwDir;
+		$this->basePath = $basePath;
+		$this->data = $data;
+		$this->formatHeaders = $formatHeaders;
+		$this->formatHtmlInjects = $formatHtmlInjects;
 	}
 
 
@@ -45,11 +47,11 @@ final class Api
 		foreach (array_keys($data = $this->findData($route)) as $format) {
 			foreach ($data[$format] ?? [] as $item) {
 				if (preg_match('/^((?:https?\:)?\/\/)(.+)$/', $item, $itemParser)) {
-					$return[] = str_replace('%path%', ($itemParser[1] === '//' ? 'https://' : '') . $itemParser[2], self::$formatHtmlInjects[$format]);
+					$return[] = str_replace('%path%', ($itemParser[1] === '//' ? 'https://' : '') . $itemParser[2], $this->formatHtmlInjects[$format]);
 				}
 			}
-			if (isset(self::$formatHtmlInjects[$format]) === true) {
-				$return[] = str_replace('%path%', Helpers::getBaseUrl() . '/assets/web-loader/' . $routePath . '.' . $format, self::$formatHtmlInjects[$format]);
+			if (isset($this->formatHtmlInjects[$format]) === true) {
+				$return[] = str_replace('%path%', Helpers::getBaseUrl() . '/assets/web-loader/' . $routePath . '.' . $format, $this->formatHtmlInjects[$format]);
 			}
 		}
 
@@ -62,12 +64,12 @@ final class Api
 		if (preg_match('/^assets\/web-loader\/(.+)$/', $path, $parser)
 			&& preg_match('/^(?<module>[a-zA-Z0-9]+)-(?<presenter>[a-zA-Z0-9]+)-(?<action>[a-zA-Z0-9]+)\.(?<format>[a-zA-Z0-9]+)$/', $parser[1], $routeParser)
 		) {
-			if (isset(self::$formatHeaders[$routeParser['format']]) === true) {
-				header('Content-Type: ' . self::$formatHeaders[$routeParser['format']]);
+			if (isset($this->formatHeaders[$routeParser['format']]) === true) {
+				header('Content-Type: ' . $this->formatHeaders[$routeParser['format']]);
 			} else {
 				throw new \RuntimeException(
 					'Content type for format "' . $routeParser['format'] . '" does not exist. '
-					. 'Did you mean "' . implode('", "', array_keys(self::$formatHeaders)) . '"?'
+					. 'Did you mean "' . implode('", "', array_keys($this->formatHeaders)) . '"?'
 				);
 			}
 
@@ -75,7 +77,7 @@ final class Api
 			if (($data = $this->findData(Helpers::formatRoute($routeParser['module'], $routeParser['presenter'], $routeParser['action']))) !== []) {
 				foreach ($data[$routeParser['format']] ?? [] as $file) {
 					echo '/* ' . $file . ' */' . "\n";
-					if (is_file($path = $this->wwwDir . '/assets/' . trim($file, '/')) === true) {
+					if (is_file($path = $this->basePath . '/' . trim($file, '/')) === true) {
 						echo file_get_contents($path);
 					}
 					echo "\n\n";
@@ -90,24 +92,11 @@ final class Api
 
 
 	/**
-	 * @param mixed[] $data
-	 * @internal used by DIC.
-	 */
-	public function setData(array $data): void
-	{
-		$this->data = $data;
-	}
-
-
-	/**
 	 * @return string[]|string[][]
 	 */
 	private function findData(?string $route = null): array
 	{
-		if ($this->data === null) {
-			AssetLoaderException::dataIsEmpty();
-		}
-		if ($route !== null) {
+		if ($this->data !== [] && $route !== null) {
 			$selectors = [];
 			if (preg_match('/^([^:]+):([^:]+):([^:]+)$/', trim($route, ':'), $routeParser)) {
 				$selectors[] = '*';
