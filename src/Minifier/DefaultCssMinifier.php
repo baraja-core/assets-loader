089f37ab80f2815753b19f0aeabe0734cd4bd1f6
Jan Barášek <janbarasek@gmail.com>
2021-01-06 11:08:45 +0100

    DefaultCssMinifier: More better compress ratio for CSS.

--- src/Minifier/DefaultCssMinifier.php
+++ src/Minifier/DefaultCssMinifier.php
@@ -9,16 +9,16 @@ final class DefaultCssMinifier implements AssetMinifier
 {
 	public function minify(string $haystack): string
 	{
-		return (string) preg_replace_callback(
-			'#[ \t\r\n]+|<(/)?(textarea|pre|script)(?=\W)#si',
-			static function ($m): string {
-				if (empty($m[2])) {
-					return ' ';
-				}
-
-				return $m[0];
-			},
+		$return = (string) preg_replace_callback(
+			'#[ \t\r\n]+|<(/)?(textarea|pre)(?=\W)#i',
+			fn (array $match): string => empty($match[2]) ? ' ' : $match[0],
 			$haystack
 		);
+		$return = (string) preg_replace('/(\w|;)\s+({|})\s+(\w|\.|#)/', '$1$2$3', $return);
+		$return = str_replace(';}', '}', $return);
+		$return = (string) preg_replace('/(\w)\s*:\s+(\w|#|-|.)/', '$1:$2', $return);
+		$return = (string) preg_replace('/\s*\/\*+[^\*]+\*+\/\s*/', '', $return);
+
+		return $return;
 	}
 }
